cb76870e92af77c393ad36b6ad75856ffff1906c
Cirine92 <1410@holbertonschool.com>
2020-06-15 23:59:50 +0000

    Bubble Sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,15 +10,20 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i, j, swaps_count = 0;
 
 	for (i = 0; i < size - 1; i++)
+	{
 		for (j = 0; j < size - i - 1; j++)
 			if (array[j] > array[j + 1])
 			{
 				swap(&array[j], &array[j + 1]);
 				print_array(array, size);
+				swaps_count++;
 			}
+		if (swaps_count == 0)
+			break;
+	}
 }
 
 /**
