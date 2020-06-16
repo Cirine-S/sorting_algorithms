1019ef70bfafa3f0ec4c192c56408d0d32fbc19c
Cirine92 <1410@holbertonschool.com>
2020-06-16 00:39:25 +0000

    trying to find out the requirement error

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -16,6 +16,7 @@ void bubble_sort(int *array, size_t size)
 		return;
 	for (i = 0; i < size - 1; i++)
 	{
+		swapped = 0;
 		for (j = 0; j < size - i - 1; j++)
 			if (array[j] > array[j + 1])
 			{
