02c25d83982cca2725fb77b375a83c1918ef4504
Cirine92 <1410@holbertonschool.com>
2020-06-16 19:53:17 +0000

    condition added

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -14,7 +14,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *curr = NULL, *tmp = *list, *head = (*list)->next;
 
-	if (!*list || !(*list)->next)
+	if (!*list || !list)
 		return;
 
 	while (head)
